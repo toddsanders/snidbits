4434e9b4daf221e78845b45fc631e4afa6e36847
toddsanders <toddsand@gmail.com>
2020-06-22 20:04:51 -0500

    snidbigts

---
+++ php/random/index.php
@@ -0,0 +1,36 @@
+<?php 
+// PHP Program to shuffle  
+// a given array 
+  
+// A function to generate 
+// a random permutation of arr[] 
+function randomize ($arr, $n) 
+{ 
+    // Start from the last element  
+    // and swap one by one. We  
+    // don't need to run for the 
+    // first element that's why i > 0 
+    for($i = $n - 1; $i >= 0; $i--) 
+    { 
+        // Pick a random index 
+        // from 0 to i 
+        $j = rand(0, $i+1); 
+  
+        // Swap arr[i] with the  
+        // element at random index 
+        $tmp = $arr[$i]; 
+        $arr[$i] = $arr[$j]; 
+        $arr[$j] = $tmp; 
+    } 
+    for($i = 0; $i < $n; $i++) 
+    echo $arr[$i]." "; 
+} 
+  
+// Driver Code 
+$arr = array(1, 2, 3, 4,  
+             5, 6, 7, 8); 
+$n = count($arr); 
+randomize($arr, $n); 
+  
+// This code is contributed by mits 
+?>
